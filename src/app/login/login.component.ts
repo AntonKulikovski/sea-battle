d5de731da07fdba65616a488f45b420f3ad709ec
Anton Kulikovski <kulikovski.a@gmail.com>
2018-09-01 12:13:29 +0300

    create login cmpt

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'cmpt-login',
+    templateUrl: './login.component.html'
+})
+export class LoginComponent {
+    public user: string;
+}
